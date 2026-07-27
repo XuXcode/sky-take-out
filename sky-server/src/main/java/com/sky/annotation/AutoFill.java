44fe3aeb017529704956a1c429c6c7687c62e5f7
XuX <841130121@qq.com>
2026-07-27 15:26:39 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/annotation/AutoFill.java
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -8,10 +8,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /*
- * 自定义注解，用于标识需要自动填充的字段
+ * 自定义注解，用于标识某个方法需要进行功能字段自动填充
  */
 @Target(ElementType.METHOD) // 表示该注解用于方法上
 @Retention(RetentionPolicy.RUNTIME) // 表示该注解在运行时生效
 public @interface AutoFill {
+    // 数据库操作类型：UPDATE INSERT
     OperationType value(); // 用于指定填充数据的操作类型,枚举值
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -80,12 +80,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         // 设置密码
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
         // 设置当前记录的创建时间和修改时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+        //employee.setCreateTime(LocalDateTime.now());
+        //employee.setUpdateTime(LocalDateTime.now());
 
         // 设置当前记录创建人id和修改人id
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        //employee.setCreateUser(BaseContext.getCurrentId());
+        //employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
@@ -118,8 +118,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = Employee.builder()
                 .status(status)
                 .id(id)
-                .updateTime(LocalDateTime.now())
-                .updateUser(BaseContext.getCurrentId())
+                //.updateTime(LocalDateTime.now())
+                //.updateUser(BaseContext.getCurrentId())
                 .build();
 
         employeeMapper.update(employee);
@@ -144,8 +144,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         // 对象属性拷贝
         BeanUtils.copyProperties(employeeDTO, employee);
 
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        //employee.setUpdateTime(LocalDateTime.now());
+        //employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.update(employee);
     }
